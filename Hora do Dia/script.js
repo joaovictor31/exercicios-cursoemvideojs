6ccd285d685bc0d97839ed60af7b92702bca8bff
João <ut7vitor60@gmail.com>
2024-04-10 20:06:15 -0300

    add js

--- Hora do Dia/script.js
+++ Hora do Dia/script.js
@@ -4,3 +4,6 @@ let dia = data.getDate();
 let hora = data.getHours();
 let minutos = data.getMinutes();
 console.log(`Agora são ${hora} horas e ${minutos} minutos`);
+
+let frase = document.querySelector(".subtitulo");
+frase.innerText = `Agora são ${hora} horas`
