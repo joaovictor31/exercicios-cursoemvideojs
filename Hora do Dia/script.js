2cdaaa716b554503d0faaf3d4afd002363df5e44
João <ut7vitor60@gmail.com>
2024-04-11 08:15:54 -0300

    Finalização Desktop

--- Hora do Dia/script.js
+++ Hora do Dia/script.js
@@ -3,7 +3,25 @@ let data = new Date();
 let dia = data.getDate();
 let hora = data.getHours();
 let minutos = data.getMinutes();
-console.log(`Agora são ${hora} horas e ${minutos} minutos`);
 
 let frase = document.querySelector(".subtitulo");
-frase.innerText = `Agora são ${hora} horas`
+let mensagem = document.querySelector(".mensagem")
+let imagem = document.querySelector(".img-manha");
+let principal = document.querySelector(".principal")
+
+
+if (hora >= 12 && hora <= 17){
+    imagem.setAttribute('src', "Ellipse 87.png");
+    document.body.style.backgroundColor = "#4A96D9"
+    principal.style.backgroundColor= "#356FA2"
+    mensagem.innerText = 'Boa Tarde'
+    frase.innerText = `Agora são ${hora} horas`
+} else if (hora >= 18 && hora <= 23){
+    imagem.setAttribute('src', "Ellipse 88.png");
+    document.body.style.backgroundColor = "#011526"
+    principal.style.backgroundColor = "#023859"
+    mensagem.innerText = 'Boa Noite';
+    frase.innerText = `Agora são ${hora} horas`
+} else{
+    frase.innerText = `Agora são ${hora} horas`
+}
